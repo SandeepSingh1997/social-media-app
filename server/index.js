bc191c07bddb521e72759e8c0d481fc1ee5f5773
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-07-06 00:17:20 +0530

    added register endpoint

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import multer from "multer";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import helmet from "helmet";
+import { fileURLToPath } from "url";
+import mongoose from "mongoose";
+
+import { register } from "./controllers/auth.js";
+
+/*Configurations*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json({ limit: "30mb" }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(morgan("common"));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assests")));
+
+/*File storage*/
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/*Routes*/
+
+app.post("/auth/register", upload.single("picture"), register);
+
+/*Mongoose setup*/
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Mongoose did not start due to : ${err}`);
+  });
